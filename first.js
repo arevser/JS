75269195c1c12519940ea2c062288d50d6fe79af
arevser <arevser@gmail.com>
2018-07-03 12:54:28 +0300

    Add cycle FOR

--- first.js
+++ first.js
@@ -9,26 +9,12 @@ let mainList = {
 	open: false
 }
 
-mainList.shopGoods[0] = prompt("Какой тип товаров будем продавать?");
-mainList.shopGoods[1] = prompt("Какой тип товаров будем продавать?");
-mainList.shopGoods[2] = prompt("Какой тип товаров будем продавать?");
+for (let i = 0; i < 4; i++) {
+	let a = prompt("Какой тип товаров будем продавать?");
+	mainList.shopGoods[i] = a;
+}
 
 console.log(mainList.budget / 30);
 
 console.log(mainList);
 
-// var yourBudjet = 500;
-// var nameStore;
-// var shopGoods = [];
-// var emploers = {
-// 	manager: "Jonh",
-// 	sellerOne: "Eugene",
-// 	sellerTwo: "Rassell"
-// }
-
-// for (var i = 0; i <= 3; i++) {
-// 	i = promptt("Какой тип товаров будем продавать?", "");
-// 	shopGoods.push(i);
-// 	console.log(i);
-// 	alert(shopGoods);
-// }
