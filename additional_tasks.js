0d3a92c20117d218673eff2798a7d6864c74cf91
arevser <arevser@gmail.com>
2018-07-03 17:07:05 +0300

    With AdditionalTasks

---
+++ additional_tasks.js
@@ -0,0 +1,26 @@
+//let x = 5; alert( x++ ); //5
+
+// [ ] + false - null + true
+//alert( [ ] + false - null + true );//NaN
+
+//let y = 1; let x = y = 2; alert(x);//2
+
+// [ ] + 1 + 2
+//alert([ ] + 1 + 2);//12
+
+//alert( "1"[0] )//1
+
+//alert(2 && 1 && null && 0 && undefined);//null
+//let a;
+//let b;
+//alert(!!( a && b ) && (a && b));//false
+
+//alert( null || 2 && 3 || 4 );//3
+
+//a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b//??
+
+//alert( +"Infinity" );//Infinity
+
+//alert("ёжик" > "яблоко")//true
+
+//alert(0 || "" || 2 || undefined || true || falsе); //2
