5eacacc3dcb07d8a9517aa827d7216733b006108
arevser <arevser@gmail.com>
2018-07-04 15:11:51 +0300

    Before second homework

--- first.js
+++ first.js
@@ -1,34 +1,61 @@
 let money,
-	name,
-	time
+name,
+time,
+price
 
 function start() {
-	money = prompt("Ваш бюджет!");
+	while (isNaN(money) || money == null || money == "") {
+		money = prompt("Ваш бюджет!");
+	}
 
-	name = prompt("Название Вашего магазина?");
+	name = prompt("Название Вашего магазина?").toUpperCase();
 	time = 19;
 }
 
-start();
+//start();
 
 let mainList = {
 	budget: money,
 	shopName: name,
 	shopGoods: [],
 	employers: {},
-	open: false
+	open: false,
+	discount: false
 }
 
-for (let i = 0; i < 3; i++) {
-	let a = prompt("Какой тип товаров будем продавать?");
-	if ((typeof(a)) === "string" && (typeof(a)) !== null && a.length < 50 && a != ""){
-		console.log("OK!");
-		mainList.shopGoods[i] = a;
-	}
-	else{
-		i = i - 1;
+function chooseGoods() {
+	for (let i = 0; i < 3; i++) {
+		let a = prompt("Какой тип товаров будем продавать?");
+		if ((typeof(a)) === "string" && (typeof(a)) !== null && a.length < 50 && a != ""){
+			console.log("OK!");
+			mainList.shopGoods[i] = a;
+		}
+		else{
+			i = i - 1;
+		}
 	}
 }
+
+//chooseGoods();
+
+function workTime(time) {
+	if (time < 0){
+		console.log("Такого не может быть!");
+	} else if (time > 8 && time < 20) {
+		console.log("Время работать!");
+	} else if (time < 24){
+		console.log("Уже слишком поздно");
+	} else {
+		console.log("В сутках только 24 часа!");
+	}
+}
+
+workTime(18);
+
+console.log(mainList.budget / 30);
+
+console.log(mainList);
+
 // let i = 0;
 // while (i < 3) {
 // 	i++;
@@ -55,17 +82,5 @@ for (let i = 0; i < 3; i++) {
 // }
 // while (i < 3);
 
-if (time < 0){
-	console.log("Такого не может быть!");
-} else if (time > 8 && time < 20) {
-	console.log("Время работать!");
-} else if (time < 24){
-	console.log("Уже слишком поздно");
-} else {
-	console.log("В сутках только 24 часа!");
-}
 
-console.log(mainList.budget / 30);
-
-console.log(mainList);
 
